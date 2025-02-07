1b7df2cb1e955c30a1af22eee69377b045e1daf7
navaj bloch <navajbloch420@gmail.com>
2025-02-07 10:18:04 +0530

    EmailVerified  set in MD screen

--- src/pages/User-authentication/EmailVerified.tsx
+++ src/pages/User-authentication/EmailVerified.tsx
@@ -2,20 +2,21 @@ import { Link } from 'react-router';
 import BasicButton from './../../stories/Buttons/BasicButton/BasicButton';
 import headerimg from './../../assets/gfheader.png';
 
-
 const EmailVerified = () => {
-    return (
-        <div className='w-screen min-h-screen bg-[#f3f4f6] flex justify-center items-center flex-col'>
-            <img className='w-[240px]' src={headerimg} />
-            <h1 className='mt-[57px] text-center font-medium text-[18px] md:text-[24px] text-[#1f2937]'>Thanks for registration!</h1>
-            <h2 className='w-[678px] text-center font-normal text-[14px] md:text-[18px] text-[#6b7280] mb-[163px]'>Email successfully verified! </h2>
-            <Link to="/">
-                <BasicButton active="enabled" label='Go to Login' />
-            </Link>
+  return (
+    <div className="w-screen min-h-screen bg-[#f3f4f6] flex justify-center items-center flex-col px-4">
+      <img className="w-[240px] sm:w-[180px]" src={headerimg} alt="Header" />
+      <h1 className="mt-[40px] text-center font-medium text-[20px] md:text-[24px] text-[#1f2937]">
+        Thanks for registration!
+      </h1>
+      <h2 className="max-w-[90%] md:max-w-[678px] text-center font-normal text-[14px] md:text-[18px] text-[#6b7280] mb-[40px] md:mb-[80px]">
+        Email successfully verified!
+      </h2>
+      <Link to="/">
+        <BasicButton active="enabled" label="Go to Login" />
+      </Link>
+    </div>
+  );
+};
 
-        </div>
-
-    )
-}
-
-export default EmailVerified
+export default EmailVerified;
--- src/pages/User-authentication/PasswordRecovery.tsx
+++ src/pages/User-authentication/PasswordRecovery.tsx
@@ -11,8 +11,10 @@ const PasswordRecovery = () => {
             <img className='w-[240px]' src={headerimg} />
             <div>
                 <h1 className='mt-[127px] mb-[15px] text-[#1F2937] text-[16px] font-medium text-center'>Password Recovery</h1>
-                <SimpleInputField active='email' label='Email' imgpath={email} />
+          
+                <SimpleInputField active='email' label='Email' imgpath={email}  />
 
+             
                 <div className='flex flex-col items-center mt-[213px]'>
                     <Link to='/recoverydone'> <BasicButton label='Submit' active='enabled' /></Link>
                     <h1 className='mt-[15px] text-[#1F2937] text-[14px] font-normal text-center'>Go back to the <span className='font-bold underline underline-offset-4'><Link to='/'>Login Page</Link></span></h1>
