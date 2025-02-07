1fae9e1fbfc3afda323f9b0193b1e5aa87fd4b86
navaj bloch <navajbloch420@gmail.com>
2025-02-07 10:20:01 +0530

    PasswordUpdated make all devices respnsive

--- src/pages/User-authentication/PasswordUpdated.tsx
+++ src/pages/User-authentication/PasswordUpdated.tsx
@@ -2,20 +2,21 @@ import BasicButton from './../../stories/Buttons/BasicButton/BasicButton';
 import headerimg from './../../assets/gfheader.png';
 import { Link } from 'react-router';
 
-
 const PasswordUpdated = () => {
-    return (
-        <div className='w-screen min-h-screen bg-[#f3f4f6] flex justify-center items-center flex-col'>
-            <img className='w-[240px]' src={headerimg} />
-            <h1 className='mt-[57px] text-center font-medium text-[24px] text-[#1f2937]'>Password updated</h1>
-            <h2 className='w-[678px] text-center font-normal text-[18px] text-[#6b7280] mb-[163px]'>You can login with the new password now.</h2>
-            <Link to='/'>
-                <BasicButton active='enabled' label='Go to Login' />
-            </Link>
+  return (
+    <div className="w-screen min-h-screen bg-[#f3f4f6] flex justify-center items-center flex-col px-4">
+      <img className="w-[240px] sm:w-[180px]" src={headerimg} alt="Header" />
+      <h1 className="mt-[40px] text-center font-medium text-[20px] md:text-[24px] text-[#1f2937]">
+        Password updated
+      </h1>
+      <h2 className="max-w-[90%] md:max-w-[678px] text-center font-normal text-[14px] md:text-[18px] text-[#6b7280] mb-[40px] md:mb-[80px]">
+        You can login with the new password now.
+      </h2>
+      <Link to="/">
+        <BasicButton active="enabled" label="Go to Login" />
+      </Link>
+    </div>
+  );
+};
 
-        </div>
-
-    )
-}
-
-export default PasswordUpdated
+export default PasswordUpdated;
